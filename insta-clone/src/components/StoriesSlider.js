a825d409100d70841bdf8f8edbda0d347656c52f
Giovanni <giovidecaro@live.it>
2022-12-13 17:09:00 +0100

    stories tab
    added the stories tab

--- insta-clone/src/components/StoriesSlider.js
+++ insta-clone/src/components/StoriesSlider.js
@@ -1,15 +1,44 @@
 import StoryIcon from "./StoryIcon";
 import storyImg from "../img/tonyProfile.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const StoriesSlider = () => {
   return (
-    <div className="flex items-center justify-center bg-red-200">
-      <div className="border-2 border-black border-solid rounded-lg p-4 flex gap-4">
-        <StoryIcon storyImg={storyImg} />
-        <StoryIcon storyImg={storyImg} />
-        <StoryIcon storyImg={storyImg} />
-        <StoryIcon storyImg={storyImg} />
-        <StoryIcon storyImg={storyImg} />
+    <div className=" flex items-center justify-center mt-6">
+      <div className="flex p-2 items-center justify-center border-solid border-2 border-black rounded-xl  w-auto max-w-[450px] ">
+        <Swiper spaceBetween={2} slidesPerView={3.6}>
+          <SwiperSlide>
+            <StoryIcon storyImg={storyImg} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <StoryIcon storyImg={storyImg} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <StoryIcon storyImg={storyImg} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <StoryIcon storyImg={storyImg} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <StoryIcon storyImg={storyImg} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <StoryIcon storyImg={storyImg} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <StoryIcon storyImg={storyImg} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <StoryIcon storyImg={storyImg} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <StoryIcon storyImg={storyImg} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <StoryIcon storyImg={storyImg} />
+          </SwiperSlide>
+        </Swiper>
       </div>
     </div>
   );
