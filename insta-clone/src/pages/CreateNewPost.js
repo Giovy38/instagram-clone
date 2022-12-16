d69263b8527ea5d7610c4c8b2c1dc84e91a034aa
Giovanni <giovidecaro@live.it>
2022-12-16 16:14:06 +0100

    create new post base page

--- insta-clone/src/pages/CreateNewPost.js
+++ insta-clone/src/pages/CreateNewPost.js
@@ -1,7 +1,11 @@
 const CreateNewPost = () => {
   return (
     <div>
-      <h1>Create New Post Page</h1>
+      <div className="border-2 border-solid border-black m-5 rounded-xl h-[60vh]">
+        <h3 className="text-center font-bold border-b-2 border-solid border-slate-400 py-5">
+          Crea un nuovo post
+        </h3>
+      </div>
     </div>
   );
 };
